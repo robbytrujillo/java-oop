fa4bedcd8da049c820e9a42b28e0d392282f94ed
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-05-05 07:36:13 +0700

    Import

--- src/programmer/zaman/now/data/Product.java
+++ src/programmer/zaman/now/data/Product.java
@@ -2,10 +2,10 @@ package programmer.zaman.now.data;
 
 public class Product {
 
-    protected String name;
-    protected int price;
+    public String name;
+    public int price;
 
-    Product (String name, int price){
+    public  Product (String name, int price){
         this.name = name;
         this.price = price;
     }
