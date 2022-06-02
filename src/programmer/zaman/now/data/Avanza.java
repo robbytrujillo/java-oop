37f81cf06578c8f6561db45d96c9b34b86fd0c20
robbyikusuma88@gmail.com <robbyikusuma88@gmail.com>
2022-06-02 12:06:24 +0700

    Intervace

---
+++ src/programmer/zaman/now/data/Avanza.java
@@ -0,0 +1,12 @@
+package programmer.zaman.now.data;
+
+public class Avanza implements Car{
+
+    public void driver() {
+        System.out.println("Avanza Drive");
+    }
+
+    public int getTire() {
+        return 4;
+    }
+}
---
+++ src/programmer/zaman/now/data/Car.java
@@ -0,0 +1,9 @@
+package programmer.zaman.now.data;
+
+public interface Car {
+
+    //public abstract void driver(); //public abstract --> tidak wajib bisa dihapus
+    void driver();
+
+    int getTire();
+}
