cc0aa0f4236dbf3dc6b963a7f39b538200173d70
robbyikusuma88@gmail.com <robbyikusuma88@gmail.com>
2022-06-05 06:45:26 +0700

    Final-Method

--- src/programmer/zaman/now/data/SosialMedia.java
+++ src/programmer/zaman/now/data/SosialMedia.java
@@ -4,11 +4,25 @@ class SosialMedia {
     String name;
 }
 
-final class Facebook extends SosialMedia{
-
-}
+//final class Facebook extends SosialMedia{
+//
+//}
 
 //Ini Error
 //class FakeFacebook extends Facebook{
 //
 //}
+
+class Facebook extends SosialMedia{
+    final void login(String username, String password){
+    //isi method
+    }
+}
+
+class FakeFacebook extends Facebook{
+    //jadi error
+    //void login(String username, String password){
+    //isi method
+    //    }
+}
+
