14a214e5d3cd2b2bda9e7135aaf256e215dc406b
robbyikusuma88@gmail.com <robbyikusuma88@gmail.com>
2022-06-05 06:29:49 +0700

    Final-Class

---
+++ src/programmer/zaman/now/data/SosialMedia.java
@@ -0,0 +1,14 @@
+package programmer.zaman.now.data;
+
+class SosialMedia {
+    String name;
+}
+
+final class Facebook extends SosialMedia{
+
+}
+
+//Ini Error
+//class FakeFacebook extends Facebook{
+//
+//}
