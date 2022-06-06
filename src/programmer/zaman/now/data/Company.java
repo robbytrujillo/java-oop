bd800835f7580513e83989eb0a70d5d1ea7e4f1f
robbyikusuma88@gmail.com <robbyikusuma88@gmail.com>
2022-06-06 22:36:34 +0700

    Inner-Class

---
+++ src/programmer/zaman/now/data/Company.java
@@ -0,0 +1,27 @@
+package programmer.zaman.now.data;
+
+public class Company {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public class Employee{
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
