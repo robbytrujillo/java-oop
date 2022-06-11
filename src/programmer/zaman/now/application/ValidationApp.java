bd04c765745f7baf3435ae924a16983a3a707669
robbyikusuma88@gmail.com <robbyikusuma88@gmail.com>
2022-06-12 02:20:08 +0700

    Membuat Exception

--- src/programmer/zaman/now/application/ValidationApp.java
+++ src/programmer/zaman/now/application/ValidationApp.java
@@ -1,6 +1,7 @@
 package programmer.zaman.now.application;
 
 import programmer.zaman.now.data.LoginRequest;
+import programmer.zaman.now.errror.ValidationException;
 import programmer.zaman.now.util.ValidationUtil;
 
 public class ValidationApp {
@@ -9,5 +10,11 @@ public class ValidationApp {
         LoginRequest loginRequest = new LoginRequest(null, null);
 
         //ValidationUtil.validate(loginRequest);
+        try {
+            ValidationUtil.validate(loginRequest);
+            System.out.println("Data Valid");
+        } catch (ValidationException exception){
+            System.out.println("Data tidak Valid: " + exception.getMessage());
+        }
     }
 }
