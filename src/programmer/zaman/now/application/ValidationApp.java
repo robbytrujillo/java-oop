b40cfe3f99d7811bb14be0589b228f7b60dcc403
robbyikusuma88@gmail.com <robbyikusuma88@gmail.com>
2022-06-12 03:06:35 +0700

    Exception

--- src/programmer/zaman/now/application/ValidationApp.java
+++ src/programmer/zaman/now/application/ValidationApp.java
@@ -9,14 +9,24 @@ public class ValidationApp {
 
         LoginRequest loginRequest = new LoginRequest(null, null);
 
-        //ValidationUtil.validate(loginRequest);
+        //Kode : Multiple Try Catch
+//        try {
+//            ValidationUtil.validate(loginRequest);
+//            System.out.println("Data Valid");
+//        } catch (ValidationException exception) {
+//            System.out.println("Terjadi Error Dengan Pesan : " + exception.getMessage());
+//        } catch (NullPointerException exception) {
+//            System.out.println("Data Null : " + exception.getMessage());
+//        }
+
+        //Kode : Multiple Try Catch (2)
         try {
             ValidationUtil.validate(loginRequest);
             System.out.println("Data Valid");
-        } catch (ValidationException exception){
+        } catch (ValidationException | NullPointerException exception) {
             System.out.println("Terjadi Error Dengan Pesan : " + exception.getMessage());
-        } catch (NullPointerException exception){
-            System.out.println("Terjadi Error Dengan Pesan : " + exception.getMessage());
+        }finally {
+            System.out.println("Error gak error, tetap di panggil");
         }
     }
 }
